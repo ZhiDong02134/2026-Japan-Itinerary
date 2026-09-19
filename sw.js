6f4403ce483384482d6f0ffb83dbcf863e650b82
Zhi Dong Dong <49508823+ZhiDong02134@users.noreply.github.com>
2026-09-19 02:32:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
   Bump CACHE when older cache namespaces must be evicted.
 */
 const CACHE_PREFIX = 'japan-2026-';
-const CACHE = `${CACHE_PREFIX}v14`;
+const CACHE = `${CACHE_PREFIX}v15`;
 const SHELL = [
   './index.html',
   './manifest.webmanifest',
